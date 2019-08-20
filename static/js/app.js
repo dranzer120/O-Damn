342da43b734bad90da61f3c74d199c895c1f81b8
Jason Tong <jasontong818@gmail.com>
2019-08-19 18:08:45 -0700

    fixed the format in csv file to work with the d3 library

--- static/js/app.js
+++ static/js/app.js
@@ -2,4 +2,18 @@ console.log("ssss")
 
 d3.csv("/static/data/ConnecticutAccidentalDeath.csv").then(function (data) {
     console.log(data)
+
+    var dateFormatSpecifier = '%m/%d/%Y';
+    var dateFormat = d3.timeFormat(dateFormatSpecifier);
+    var dateFormatParser = d3.timeParse(dateFormatSpecifier);
+    var numberFormat = d3.format('.2f');
+
+    data.forEach(function (d) {
+        d.parsed_date = dateFormatParser(d.Date);
+        d.month = d3.timeMonth(d.parsed_date); // pre-calculate month for better performance
+        d.age = +d.Age;
+
+    });
+
+    console.log(data[0].parsed_date)
 })
